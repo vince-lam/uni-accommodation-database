bc6b245635307c825317a0716b6daacfaba72776
Vincent Lam <vincentlammy8@gmail.com>
2022-08-12 20:47:41 +0100

    Add backend login php file.

---
+++ php/login.php
@@ -0,0 +1,21 @@
+<?php
+$username = $_REQUEST ['username'];
+$password = $_REQUEST ['password'];
+$conn=mysqli_connect('localhost','root','','yahuas');
+
+$sql = "SELECT * FROM login where username = '$username' and password = '$password'"; 
+
+$result = mysqli_query($conn, $sql);  
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+$count = mysqli_num_rows($result);  
+
+
+if($count == 1){  
+    header('Location: common/student.html');
+}  
+else{  
+    echo "<h1> Login failed. Invalid username or password.</h1>";  
+}     
+
+$conn->close();
+?>
